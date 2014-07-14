32d60cca35dcd9962c972d55bdc34d2d8605f2ce
sivakumar <shiva.sms65@gmail.com>
2014-07-14 19:56:33 +0530

    changes for assets precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -76,6 +76,8 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.assets.initialize_on_precompile = false
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
